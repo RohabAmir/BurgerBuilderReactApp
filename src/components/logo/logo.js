4ccbabf365861e3fc8c56378bc650bd8594c3616
RohabAmir <rohabamir2@gmail.com>
2023-08-07 15:19:46 +0500

    More Responsive Adjustments for Mobile devices

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -5,7 +5,7 @@ import classes from './logo.module.css';
 import burgerLogo from "../../assets/images/28.1 burger-logo.png.png";
 
 const logo = (props) =>(
-    <div className={classes.Logo}>
+    <div className={classes.Logo}> 
         <img src={burgerLogo} alt="MyBurger"/>
     </div>
 
