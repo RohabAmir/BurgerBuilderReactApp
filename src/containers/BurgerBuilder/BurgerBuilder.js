ce99830c3049bb07131072b721f2508de60e0564
RohabAmir <rohabamir2@gmail.com>
2023-08-08 12:23:03 +0500

    Sending Http Request to Backend and getting the order's status there

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,7 +94,8 @@ class burgerBuilder extends Component{
                     zipCode: '6303',
                     country: 'Pakistan'
                 },
-                email: 'Rohabamir2@gmail.com',   
+                email: 'Rohabamir2@gmail.com',
+                phoneNo: '0320-******'   
             },
             deliveryMethod: "fastest"
         }
