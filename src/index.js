0c9f2a7bbcfc13b4fc919153fcded80e3a50a0e7
RohabAmir <rohabamir2@gmail.com>
2023-08-15 17:48:33 +0500

    Building up the Reducer's store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'react';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
 
-const store= createStore(reducer);
+const store= createStore (reducer);
 
 const app=(
 
@@ -23,6 +23,7 @@ const app=(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( app
 );
+
 
 registerServiceWorker();
 
