2384d4391b7f85b78e5e9a9b9460f1dd2878cc43
RohabAmir <rohabamir2@gmail.com>
2023-08-15 18:20:35 +0500

    Coonecting the BurgerBuilder Container to our store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 class burgerBuilder extends Component{
 
     state = { //defining and manipulating th state in the main container 
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -42,7 +41,7 @@ class burgerBuilder extends Component{
             .reduce((sum, el)=>{
                 return sum + el;
             },0);
-        this.setState({purchasable : sum > 0}); //manipulating the state 
+        return  sum > 0 ; //manipulating the state 
     }
     
     purchaseHandler = () => {
@@ -89,7 +88,7 @@ class burgerBuilder extends Component{
                     ingredientAdded={this.props.onIngredientAdded}
                     ingredientRemoved={this.props.onIngredientRemoved}
                     disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
+                    purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
                     price={this.props.price} />
             </Aux>
