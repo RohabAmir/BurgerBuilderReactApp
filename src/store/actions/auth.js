ff1b58d3288cd9dbafb66de24d6a1000a94e00a8
RohabAmir <rohabamir2@gmail.com>
2023-08-18 16:44:45 +0500

    Logging User Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -17,6 +17,18 @@ export const authFail = (error) =>{
     return{
         type: actionTypes.AUTH_FAIL,
         error: error
+    };  
+};
+export const logout=()=>{
+    return{
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+export const checkAuthTimeout=(expirationTime)=>{
+    return dispatch =>{
+        setTimeout(()=>{
+            dispatch(logout());
+        },expirationTime * 1000)
     };
 };
 
@@ -36,6 +48,7 @@ export const auth= (email,password, isSignUp)=>{ //handling Authentication Async
             .then(response =>{
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
                 dispatch(authFail(err.response.data.error));
